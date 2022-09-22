d32eecabed9eb15259fd645b54ac9efba23e817b
hschiebold <holger.schiebold@gmx.de>
2022-09-22 11:32:58 +0200

    Update ConfirmPaymentIntentAction.php
    only payment_intent with status  requires_confirmation or requires_action can be confirmed

--- vendor/payum/stripe/Payum/Stripe/Action/Api/ConfirmPaymentIntentAction.php
+++ vendor/payum/stripe/Payum/Stripe/Action/Api/ConfirmPaymentIntentAction.php
@@ -69,7 +69,9 @@ class ConfirmPaymentIntentAction implements ActionInterface, ApiAwareInterface
 
             $intent = PaymentIntent::retrieve($model['payment_intent']);
 
-            $intent->confirm();
+            if ($intent['status'] == 'requires_confirmation' || $intent['status'] == 'requires_action') {
+                $intent->confirm();
+            }
 
             $model->replace($intent->__toArray(true));
 
