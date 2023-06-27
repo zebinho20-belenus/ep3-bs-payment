a88ab6d184a9f5831858c3543bbcd0a3930d22ce
Amber Jain <amberjain1234@gmail.com>
2023-06-27 21:22:00 +1000

    composer.lock fix

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit80187b261285ce6ecce57578ecc7afb2
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
